804c8e561cd6ae26881a4b4682618499b18bed60
SergioCltn <sgs98xd@gmail.com>
2024-12-03 10:12:27 +0100

    add json formatter

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -32,6 +32,8 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      json = { 'prettier' },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
