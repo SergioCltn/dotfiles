b518e52a308b85e9bf2bd496802bbae5739d8367
SergioCltn <sgs98xd@gmail.com>
2026-01-15 02:46:51 +0100

    add opencode to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -552,6 +552,7 @@ require('lazy').setup({
   require 'plugins.mini',
   require 'plugins.dadbod',
   require 'plugins.copilot',
+  require 'plugins.opencode',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
