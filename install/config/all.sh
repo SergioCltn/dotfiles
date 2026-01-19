8baadafb478ed6fc4bc7fdf579fffb302592c443
SergioCltn <sgs98xd@gmail.com>
2026-01-19 20:03:05 +0100

    typo

--- install/config/all.sh
+++ install/config/all.sh
@@ -3,7 +3,7 @@ run_logged $DOTFILES_INSTALL/config/increase-sudo-tries.sh # this is for hyprloc
 run_logged $DOTFILES_INSTALL/config/increase-lockout-limit.sh
 run_logged $DOTFILES_INSTALL/config/ssh-flakiness.sh #problem with ssh connections being unrelibable and fail intermittently
 
-run_logged $DOTFILES_INSTALL/config/mpacmanise-work.sh
+run_logged $DOTFILES_INSTALL/config/mise-work.sh
 run_logged $DOTFILES_INSTALL/config/fix-powerprofilesctl-shebang.sh
 run_logged $DOTFILES_INSTALL/config/docker.sh
 run_logged $DOTFILES_INSTALL/config/mimetypes.sh
