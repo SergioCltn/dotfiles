d7b2fdf2735ba67bb3e3e982079bf52bcc13f123
SergioCltn <sgs98xd@gmail.com>
2026-01-21 18:23:25 +0100

    remove boot

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,4 +1,4 @@
-source $DOTFILES_INSTALL/helpers/chroot.sh
 source $DOTFILES_INSTALL/helpers/presentation.sh
+source $DOTFILES_INSTALL/helpers/chroot.sh
 source $DOTFILES_INSTALL/helpers/errors.sh
 source $DOTFILES_INSTALL/helpers/logging.sh
