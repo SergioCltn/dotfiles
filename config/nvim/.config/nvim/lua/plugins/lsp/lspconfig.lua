ca4b3aedc7176ff840895a84f1ff976e14f79979
SergioCltn <sgs98xd@gmail.com>
2026-02-10 23:59:00 +0100

    fix(nvim): remove unnecessary version check

--- config/nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ config/nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -45,12 +45,7 @@ return {
           ---@param bufnr? integer
           ---@return boolean
           local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              ---@diagnostic disable-next-line: param-type-mismatch
-              return client.supports_method(method, { bufnr = bufnr })
-            end
+            return client:supports_method(method, bufnr)
           end
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
